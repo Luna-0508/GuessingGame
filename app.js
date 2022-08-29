50492f48cb1ec01a95536e67ba727fe68db63a9f
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-08-29 21:38:38 +0200

    Loop for too high/low number

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ while (!maximum) {
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 
 console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+while (guess !== targetNum) {
+  if (guess > targetNum) {
+    guess = parseInt(prompt("Too high! Enter a new guess:"));
+  } else {
+    guess = parseInt(prompt("Too low! Enter a new guess:"));
+  }
+}
+
+console.log("YOU GOT IT!");
